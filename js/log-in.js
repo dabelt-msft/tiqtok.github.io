e5f243db084053d8151f0870b7331ca25b9f99de
Nick Spinosa <nickspinosa1022@gmail.com>
2016-07-01 16:02:44 -0400

    adds jquery validation

--- js/log-in.js
+++ js/log-in.js
@@ -4,11 +4,33 @@ var user = {};
 user.id={};
 
 user.logIn= function(e){
-  var email = $("#email").val();
-  var password = $("#password").val();
-  user.id["email"]=email;
-  user.id["password"]=password;
-  $("#form")[0].reset();
+  e.preventDefault();
+  if($("#form").valid()){
+    var email = $("#email").val();
+    var password = $("#password").val();
+    user.id["email"]=email;
+    user.id["password"]=password;
+    $("#form")[0].reset();
+  }
 }
 
-$("#log-in-button").on("click", user.logIn());
+$("#log-in-button").on("click", user.logIn);
+
+$().ready(()=>{
+    $("#form").validate({
+      rules: {
+        email: {
+          required:true,
+          email: true
+        },
+        password: {
+          required: true,
+          minlength: 8
+        }
+      },
+      messages: {
+        email: "Please enter a valid email adress",
+        password: "password must be at least 8 characters"
+      }
+    });
+})
